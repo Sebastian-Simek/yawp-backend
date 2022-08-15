0cf7e81dfa8466203c3b4d150e0c2fd852b96468
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-08-15 14:03:02 -0700

    added sql tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,37 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS reviews;
+DROP TABLE IF EXISTS restuarants;
+
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email VARCHAR,
+    username VARCHAR NOT NULL,
+    password_hash TEXT NOT NULL
+);
+
+CREATE TABLE reviews (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    review VARCHAR NOT NULL,    
+    user_id BIGINT,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE restuarants(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR,
+    location VARCHAR,
+    review_id BIGINT,
+    FOREIGN KEY (review_id) REFERENCES reviews (id)
+);
+
+INSERT INTO users (
+    email,
+    username,
+    password_hash
+)
+VALUES
+('test@test.com', 'MR. Test', 'not@HasH'),
+('teeeest@tesasdft.com', 'MRasdf. Teasdfst', 'notasdf@HasH');
+
