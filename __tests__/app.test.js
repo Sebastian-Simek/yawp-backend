4d549d41ec27770714e327a1c9cb35b206554f23
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-08-15 15:10:34 -0700

    added and passed first test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,16 +1,32 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const mockUser = {
+  email: 'testemail@test.com',
+  username: 'testUser',
+  password: '123456'
+};
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
-  });
   afterAll(() => {
     pool.end();
+  });
+  it('#POST /api/v1/users creates and signs in a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { email, username } = mockUser;
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      message: 'Signed in successfully',
+      user: {
+        id: expect.any(String),
+        email,
+        username
+      }
+    });
   });
 });
